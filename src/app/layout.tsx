78b6536a0a91991146ec5dc615dcaaaa51c53f5f
wDvy <69913344+wDvy@users.noreply.github.com>
2026-07-05 16:13:59 -0400

    suppress warning (lol)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,12 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+    <html suppressHydrationWarning lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className="min-h-full flex flex-col">
+      <body suppressHydrationWarning className="min-h-full flex flex-col">
         <ThemeProvider>
           {children}
         </ThemeProvider>
