6092ee5ec4308b415a752a09aa263b34f4c4be14
Devin Ward <devin.ward@louisville.edu>
2026-08-09 19:32:46 -0400

    add gazette back to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -49,7 +49,9 @@ export default function Footer() {
                 <Link
                   href="/gazette"
                   className="text-sm text-zinc-600 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-50"
-                ></Link>
+                >
+                  Gazette
+                </Link>
               </li>
             </ul>
           </div>
